5efbef5a410b3e29be2d4e6c4a5f57ecc5ed72a9
Mark Furland <mtfurlan@i3detroit.org>
2021-09-19 14:09:58 -0400

    cleanup, comments, fix newlines

--- new_usb_to_xt.ino
+++ new_usb_to_xt.ino
@@ -24,7 +24,7 @@
 #define CLOCKPIN   8
 
 Adafruit_DotStar strip = Adafruit_DotStar(
-  NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
+        NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
 
 
 // Require keyboard control library
@@ -35,199 +35,202 @@ USBHost usb;
 
 
 class KeyboardRaw : public KeyboardReportParser {
-public:
-  KeyboardRaw(USBHost &usb) : hostKeyboard(&usb) {
-    hostKeyboard.SetReportParser(0, this);
-  };
+    public:
+        KeyboardRaw(USBHost &usb) : hostKeyboard(&usb) {
+            hostKeyboard.SetReportParser(0, this);
+        };
 
-  void Parse(HID *hid, uint32_t is_rpt_id, uint32_t len, uint8_t *buf);
+        void Parse(HID *hid, uint32_t is_rpt_id, uint32_t len, uint8_t *buf);
 
-private:
-  HIDBoot<HID_PROTOCOL_KEYBOARD> hostKeyboard;
+    private:
+        HIDBoot<HID_PROTOCOL_KEYBOARD> hostKeyboard;
 };
 
 bool canConvertUSB2XT(uint8_t usbScanCode) {
-  return usbScanCode <= 69; //TODO: figure it out
+    return usbScanCode <= 69; //TODO: figure it out
 }
+
 void xt_write(unsigned char value)
 {
-   //while (digitalRead(xt_clk) != HIGH) ;
-   unsigned char bits[8] ;
-   byte p = 0 ;
-   byte j = 0 ;
-   for (j=0 ; j < 8; j++)
-   {
-     if (value & 1) bits[j] = 1 ;
-     else bits[j] = 0 ;
-     value = value >> 1 ;
-   }
-   digitalWrite(xt_clk, LOW) ;
-   digitalWrite(xt_data, HIGH) ;
-   delayMicroseconds(120) ;
-   digitalWrite(xt_clk, HIGH) ;
-   delayMicroseconds(66) ;
-   digitalWrite(xt_clk, LOW) ;
-   delayMicroseconds(30) ;
-   digitalWrite(xt_clk, HIGH) ;
-   byte i = 0 ;
-   for (i=0; i < 8; i++)
-   {
-      digitalWrite(xt_clk, HIGH) ;
-      digitalWrite(xt_data, bits[p]) ;
-      delayMicroseconds(95) ;
-      digitalWrite(xt_clk, LOW) ;
-      digitalWrite(xt_data, LOW) ;
-      p++ ;
-   }
-   digitalWrite(xt_clk, HIGH) ;
-   digitalWrite(xt_data, LOW) ;
-   delay(1) ;
+    //while (digitalRead(xt_clk) != HIGH) ;
+    unsigned char bits[8] ;
+    byte p = 0 ;
+    byte j = 0 ;
+    for (j=0 ; j < 8; j++) {
+        if (value & 1) bits[j] = 1 ;
+        else bits[j] = 0 ;
+        value = value >> 1 ;
+    }
+    digitalWrite(xt_clk, LOW) ;
+    digitalWrite(xt_data, HIGH) ;
+    delayMicroseconds(120) ;
+    digitalWrite(xt_clk, HIGH) ;
+    delayMicroseconds(66) ;
+    digitalWrite(xt_clk, LOW) ;
+    delayMicroseconds(30) ;
+    digitalWrite(xt_clk, HIGH) ;
+    byte i = 0 ;
+    for (i=0; i < 8; i++) {
+        digitalWrite(xt_clk, HIGH) ;
+        digitalWrite(xt_data, bits[p]) ;
+        delayMicroseconds(95) ;
+        digitalWrite(xt_clk, LOW) ;
+        digitalWrite(xt_data, LOW) ;
+        p++ ;
+    }
+    digitalWrite(xt_clk, HIGH) ;
+    digitalWrite(xt_data, LOW) ;
+    delay(1) ;
 }
+
 void key(int usbScan, bool down) {
-  if(DEBUG) {
-    Serial.print(down ? "pressed: " : "unpressed: ");
-    Serial.print("usbOrig: ");
-    Serial.print(usbScan);
-  }
-  // the array is sparse, and this is stupid. Sorry.
-  if(usbScan == 154) usbScan = 100;
-  if(usbScan >= 224) usbScan -= 123;
-  uint8_t xt_first = usbscan_xt[usbScan];
-  switch(xt_first) {
-    case 0xe0: // two byte
-      //TODO: support two byte stuff
-      //sparse again, sorry
-      switch(usbScan) {
-        case 70:
-          usbScan =- 70;
-          break;
-        case 73 ... 82:
-          usbScan -= 72;
-          break;
-        case 84:
-          usbScan = 11;
-          break;
-        case 88:
-          usbScan = 12;
-          break;
-        //it's the 2xx range, which already had 123 subtracted
-        case 227 ... 228:
-          usbScan -= 91;
-          break;
-        case 230 ... 231:
-          usbScan -= 92;
-          break;
-      }
-      switch(usbscan_xt_byte2[usbScan]) {
-        case 0x1d: // RCtrl
-          xt_first = 0x1d;
-          break;
-        case 0x38: // RAlt
-          xt_first = 0x38;
-          break;
-        case 0x1c: // kp-enter
-          xt_first = 0x1c;
-          break;
-      }
-      break;
-    case 0xe1: // three byte(pause)
-      //TODO: ignoring pause
-      break;
-  }
-  if(DEBUG) {
-    Serial.print(", usbFinal: ");
-    Serial.print(usbScan);
-    Serial.print(", xt: 0x");
-    Serial.println(xt_first, HEX);
-  }
-  if(down) {
-    xt_write(xt_first);
-  } else {
-    xt_write(xt_first | 0x80);
-  }
+    if(DEBUG) {
+        Serial.print(down ? "pressed: " : "unpressed: ");
+        Serial.print("usbOrig: ");
+        Serial.print(usbScan);
+    }
+    // the array is sparse, and this is stupid. Sorry.
+    // TODO: rework array map
+    if(usbScan == 154) usbScan = 100;
+    if(usbScan >= 224) usbScan -= 123;
+    uint8_t xt_first = usbscan_xt[usbScan];
+    switch(xt_first) {
+        case 0xe0: // two byte
+            //sparse again, sorry
+            switch(usbScan) {
+                case 70:
+                    usbScan =- 70;
+                    break;
+                case 73 ... 82:
+                    usbScan -= 72;
+                    break;
+                case 84:
+                    usbScan = 11;
+                    break;
+                case 88:
+                    usbScan = 12;
+                    break;
+                    //it's the 2xx range, which already had 123 subtracted
+                case 227 ... 228:
+                    usbScan -= 91;
+                    break;
+                case 230 ... 231:
+                    usbScan -= 92;
+                    break;
+            }
+            //TODO: support two byte stuff instead of reampping some of it
+            switch(usbscan_xt_byte2[usbScan]) {
+                case 0x1d: // RCtrl
+                    xt_first = 0x1d;
+                    break;
+                case 0x38: // RAlt
+                    xt_first = 0x38;
+                    break;
+                case 0x1c: // kp-enter
+                    xt_first = 0x1c;
+                    break;
+            }
+            break;
+        case 0xe1: // three byte(pause)
+            //TODO: ignoring pause
+            break;
+    }
+    if(DEBUG) {
+        Serial.print(", usbFinal: ");
+        Serial.print(usbScan);
+        Serial.print(", xt: 0x");
+        Serial.println(xt_first, HEX);
+    }
+    if(down) {
+        xt_write(xt_first);
+    } else {
+        xt_write(xt_first | 0x80);
+    }
 }
 void KeyboardRaw::Parse(HID *hid, uint32_t is_rpt_id, uint32_t len, uint8_t *buf)
 {
-  /*
-   * USB HID keyboard report
-Offset Description
-0      Modifier keys status.
-1      Reserved field.
-2      Keypress #1.
-3      Keypress #2.
-4      Keypress #3.
-5      Keypress #4.
-6      Keypress #5.
-7      Keypress #6.
-   */
+    /*
+     * USB HID keyboard report
+     Offset Description
+     0      Modifier keys status.
+     1      Reserved field.
+     2      Keypress #1.
+     3      Keypress #2.
+     4      Keypress #3.
+     5      Keypress #4.
+     6      Keypress #5.
+     7      Keypress #6.
+     ...
+     N-1    Keypress #N.
+     */
 
-  if(DEBUG) {
-    Serial.println("keyboard parse buf: ");
-    for(int i=0; i<len; ++i) {
-      Serial.print("  ");
-      Serial.print(i);
-      Serial.print(": ");
-      Serial.print("  0x");
-      Serial.println(buf[i], HEX);
-    }
-  }
-
-  //curmod/prevmod are for modifiers
-  static uint8_t prevMod = 0;
-  uint8_t curMod = buf[0];
-
-  for (int i = 0; i < 8; i++) {
-    if(curMod & (1 << i)) {
-      if(!(prevMod & (1 << i))) {
-        // this lets us deal with multi-byte keys in one place converting the usb modifiers
-        // to codes
-        key(usbModLookup[i], true);
-      } // else continued being pressed
-    } else if(prevMod & (1 << i)) {
-        key(usbModLookup[i], false);
-    }
-  }
-
-  //buf[2] to buf[len] are a list of pressed keys
-  static uint8_t* curScans = NULL;
-  static uint8_t* prevScans = NULL;
-  static size_t scanLen = 6; //TODO: check len to see if it's bigger and realloc or whatever
-  if(curScans == NULL) {
-    curScans = (uint8_t*)calloc(scanLen, 1);
-  }
-  if(prevScans == NULL) {
-    prevScans = (uint8_t*)calloc(scanLen, 1);
-  }
-  memcpy (curScans, &buf[2], len-2);
-  int i = 0;
-  int j = 0;
-  uint8_t p;
-  uint8_t c;
-  while (i < 7 && j < 7) {
-    p = prevScans[j];
-    c = curScans[i];
-    if (p != c) {
-      if (p == 0) { // New key, so send "down"
-        if (canConvertUSB2XT(c)) {
-          key(c, true);
+    if(DEBUG) {
+        Serial.println("keyboard parse buf: ");
+        for(int i=0; i<len; ++i) {
+            Serial.print("  ");
+            Serial.print(i);
+            Serial.print(": ");
+            Serial.print("  0x");
+            Serial.println(buf[i], HEX);
         }
-        i++;
-      } else { // Previous key down, so send "up"
-        if (canConvertUSB2XT(c)) {
-          key(p, false);
-        }
-        j++;
-      }
-    } else {
-      // Matching keys, advance both
-      i++;
-      j++;
     }
-  }
 
-  // Save current into previous
-  prevMod = curMod;
-  memcpy (prevScans, curScans, scanLen);
+    //curmod/prevmod are for modifiers
+    static uint8_t prevMod = 0;
+    uint8_t curMod = buf[0];
+
+    for (int i = 0; i < 8; i++) {
+        if(curMod & (1 << i)) {
+            if(!(prevMod & (1 << i))) {
+                // this lets us deal with multi-byte keys in one place converting the usb modifiers
+                // to codes
+                key(usbModLookup[i], true);
+            } // else continued being pressed
+        } else if(prevMod & (1 << i)) {
+            key(usbModLookup[i], false);
+        }
+    }
+
+    //buf[2] to buf[len] are a list of pressed keys
+    static uint8_t* curScans = NULL;
+    static uint8_t* prevScans = NULL;
+    static size_t scanLen = 6; //TODO: check len to see if it's bigger and realloc or whatever
+    if(curScans == NULL) {
+        curScans = (uint8_t*)calloc(scanLen, 1);
+    }
+    if(prevScans == NULL) {
+        prevScans = (uint8_t*)calloc(scanLen, 1);
+    }
+    memcpy (curScans, &buf[2], len-2);
+    int i = 0;
+    int j = 0;
+    uint8_t p;
+    uint8_t c;
+    while (i < 7 && j < 7) {
+        p = prevScans[j];
+        c = curScans[i];
+        if (p != c) {
+            if (p == 0) { // New key, so send "down"
+                if (canConvertUSB2XT(c)) {
+                    key(c, true);
+                }
+                i++;
+            } else { // Previous key down, so send "up"
+                if (canConvertUSB2XT(c)) {
+                    key(p, false);
+                }
+                j++;
+            }
+        } else {
+            // Matching keys, advance both
+            i++;
+            j++;
+        }
+    }
+
+    // Save current into previous
+    prevMod = curMod;
+    memcpy (prevScans, curScans, scanLen);
 }
 
 // Attach keyboard controller to USB
@@ -235,63 +238,63 @@ KeyboardRaw keyboard(usb);
 
 void setup()
 {
-  if(DEBUG) {
-    Serial.begin(9600);
-    Serial.println("Program started");
-  }
+    if(DEBUG) {
+        Serial.begin(9600);
+        Serial.println("Program started");
+    }
 
-  strip.begin(); // Initialize pins for output
-  strip.show();  // Turn all LEDs off ASAP
+    strip.begin(); // Initialize pins for output
+    strip.show();  // Turn all LEDs off ASAP
 
-  pinMode(xt_clk, OUTPUT) ;
-  pinMode(xt_data, OUTPUT) ;
-  digitalWrite(xt_clk, HIGH) ;
-  digitalWrite(xt_data, HIGH) ;
+    pinMode(xt_clk, OUTPUT) ;
+    pinMode(xt_data, OUTPUT) ;
+    digitalWrite(xt_clk, HIGH) ;
+    digitalWrite(xt_data, HIGH) ;
 
-  delay(200);
+    delay(200);
 }
 
 void loop()
 {
 
-  // Process USB tasks
+    // Process USB tasks
 
-  usb.Task();
+    usb.Task();
 
-  static uint32_t lastUSBstate;
-  uint32_t currentUSBstate = usb.getUsbTaskState();
-  if (lastUSBstate != currentUSBstate)
-  {
-    switch (currentUSBstate)
+    static uint32_t lastUSBstate;
+    uint32_t currentUSBstate = usb.getUsbTaskState();
+    if (lastUSBstate != currentUSBstate)
     {
-    case USB_ATTACHED_SUBSTATE_SETTLE:
-      strip.setPixelColor(0, 0x280028); // purple
-      strip.show();
-      break;
-    case USB_DETACHED_SUBSTATE_WAIT_FOR_DEVICE:
-      strip.setPixelColor(0, 0x000028); // blue
-      strip.show();
-      break;
-    case USB_ATTACHED_SUBSTATE_RESET_DEVICE:
-      strip.setPixelColor(0, 0x282800); // yellow
-      strip.show();
-      break;
-    case USB_ATTACHED_SUBSTATE_WAIT_RESET_COMPLETE:
-      strip.setPixelColor(0, 0x281400); // orange
-      strip.show();
-      break;
-    case USB_STATE_CONFIGURING:
-      strip.setPixelColor(0, 0x002828); // cyan
-      strip.show();
-      break;
-    case USB_STATE_RUNNING:
-      strip.setPixelColor(0, 0x005500); // green, bright
-      strip.show();
-      //resetkb ();
-      strip.setPixelColor(0, 0x000000); // off
-      strip.show();
-      break;
+        switch (currentUSBstate)
+        {
+            case USB_ATTACHED_SUBSTATE_SETTLE:
+                strip.setPixelColor(0, 0x280028); // purple
+                strip.show();
+                break;
+            case USB_DETACHED_SUBSTATE_WAIT_FOR_DEVICE:
+                strip.setPixelColor(0, 0x000028); // blue
+                strip.show();
+                break;
+            case USB_ATTACHED_SUBSTATE_RESET_DEVICE:
+                strip.setPixelColor(0, 0x282800); // yellow
+                strip.show();
+                break;
+            case USB_ATTACHED_SUBSTATE_WAIT_RESET_COMPLETE:
+                strip.setPixelColor(0, 0x281400); // orange
+                strip.show();
+                break;
+            case USB_STATE_CONFIGURING:
+                strip.setPixelColor(0, 0x002828); // cyan
+                strip.show();
+                break;
+            case USB_STATE_RUNNING:
+                strip.setPixelColor(0, 0x005500); // green, bright
+                strip.show();
+                //resetkb ();
+                strip.setPixelColor(0, 0x000000); // off
+                strip.show();
+                break;
+        }
+        lastUSBstate = currentUSBstate;
     }
-    lastUSBstate = currentUSBstate;
-  }
 }
--- xt_defines.h
+++ xt_defines.h
@@ -6,150 +6,150 @@
 // go from usb modifier to "usb code" from https://www.scs.stanford.edu/10wi-cs140/pintos/specs/kbd/scancodes-9.html
 const uint8_t usbModLookup[] =
 {
-  224,             // Left control
-  225,            // Left Shift
-  226,             // Left Alt
-  227,             // Left Windows
-  228,             // Right control
-  229,            // right shift
-  230,             // Right Alt
-  231              // Right Windows
+    224,             // Left control
+    225,            // Left Shift
+    226,             // Left Alt
+    227,             // Left Windows
+    228,             // Right control
+    229,            // right shift
+    230,             // Right Alt
+    231              // Right Windows
 };
 
 
 const uint8_t usbscan_xt_byte2[] =
 {
-  0x37, // usb 70 key PrtScr
-0x52, // usb 73 key Insert
-0x47, // usb 74 key Home
-0x49, // usb 75 key PgUp
-0x53, // usb 76 key Delete
-0x4f, // usb 77 key End
-0x51, // usb 78 key PgDn
-0x4d, // usb 79 key Right
-0x4b, // usb 80 key Left
-0x50, // usb 81 key Down
-0x48, // usb 82 key Up
-0x35, // usb 84 key KP-/
-0x1c, // usb 88 key KP-Enter
-0x5b, // usb 227 key LWin (USB: LGUI)
-0x1d, // usb 228 key RCtrl
-0x38, // usb 230 key RAlt
-0x5c, // usb 231 key RWin (USB: RGUI)
+    0x37, // usb 70 key PrtScr
+    0x52, // usb 73 key Insert
+    0x47, // usb 74 key Home
+    0x49, // usb 75 key PgUp
+    0x53, // usb 76 key Delete
+    0x4f, // usb 77 key End
+    0x51, // usb 78 key PgDn
+    0x4d, // usb 79 key Right
+    0x4b, // usb 80 key Left
+    0x50, // usb 81 key Down
+    0x48, // usb 82 key Up
+    0x35, // usb 84 key KP-/
+    0x1c, // usb 88 key KP-Enter
+    0x5b, // usb 227 key LWin (USB: LGUI)
+    0x1d, // usb 228 key RCtrl
+    0x38, // usb 230 key RAlt
+    0x5c, // usb 231 key RWin (USB: RGUI)
 
 };
 
 const uint8_t usbscan_xt[] =
 {
-  0,
-  0,
-  0,
-  0,
-0x1e, // usb 4 key A
-0x30, // usb 5 key B
-0x2e, // usb 6 key C
-0x20, // usb 7 key D
-0x12, // usb 8 key E
-0x21, // usb 9 key F
-0x22, // usb 10 key G
-0x23, // usb 11 key H
-0x17, // usb 12 key I
-0x24, // usb 13 key J
-0x25, // usb 14 key K
-0x26, // usb 15 key L
-0x32, // usb 16 key M
-0x31, // usb 17 key N
-0x18, // usb 18 key O
-0x19, // usb 19 key P
-0x10, // usb 20 key Q
-0x13, // usb 21 key R
-0x1f, // usb 22 key S
-0x14, // usb 23 key T
-0x16, // usb 24 key U
-0x2f, // usb 25 key V
-0x11, // usb 26 key W
-0x2d, // usb 27 key X
-0x15, // usb 28 key Y
-0x2c, // usb 29 key Z
-0x02, // usb 30 key 1 !
-0x03, // usb 31 key 2 @
-0x04, // usb 32 key 3 #
-0x05, // usb 33 key 4 $
-0x06, // usb 34 key 5 % E
-0x07, // usb 35 key 6 ^
-0x08, // usb 36 key 7 &
-0x09, // usb 37 key 8 *
-0x0a, // usb 38 key 9 (
-0x0b, // usb 39 key 0 )
-0x1c, // usb 40 key Enter
-0x01, // usb 41 key Esc
-0x0e, // usb 42 key Backspace
-0x0f, // usb 43 key Tab
-0x39, // usb 44 key space
-0x0c, // usb 45 key - _
-0x0d, // usb 46 key = +
-0x1a, // usb 47 key [ {
-0x1b, // usb 48 key ] }
-0x2b, // usb 49 key \ |
-0x00, // usb 50 key non-US-1
-0x27, // usb 51 key ; :
-0x28, // usb 52 key ' "
-0x29, // usb 53 key ` ~
-0x33, // usb 54 key , <
-0x34, // usb 55 key . >
-0x35, // usb 56 key / ?
-0x3a, // usb 57 key CapsLock
-0x3b, // usb 58 key F1
-0x3c, // usb 59 key F2
-0x3d, // usb 60 key F3
-0x3e, // usb 61 key F4
-0x3f, // usb 62 key F5
-0x40, // usb 63 key F6
-0x41, // usb 64 key F7
-0x42, // usb 65 key F8
-0x43, // usb 66 key F9
-0x44, // usb 67 key F10
-0x57, // usb 68 key F11
-0x58, // usb 69 key F12
-0x37, // usb 70 key PrtScr eo-37
-0x46, // usb 71 key ScrollLock
-0xe1, // usb 72 key Pause e1-1d-45
-0x52, // usb 73 key Insert eo-52
-0x47, // usb 74 key Home eo-47
-0x49, // usb 75 key PgUp eo-49
-0x53, // usb 76 key Delete eo-53
-0x4f, // usb 77 key End eo-4f
-0x51, // usb 78 key PgDn eo-51
-0x4d, // usb 79 key Right eo-4d
-0x4b, // usb 80 key Left eo-4b
-0x50, // usb 81 key Down eo-50
-0x48, // usb 82 key Up eo-48
-0x45, // usb 83 key NumLock
-0x35, // usb 84 key KP-/ eo-35
-0x37, // usb 85 key KP-*
-0x4a, // usb 86 key KP--
-0x4e, // usb 87 key KP-+
-0x1c, // usb 88 key KP-Enter eo-1c
-0x4f, // usb 89 key KP-1 / End
-0x50, // usb 90 key KP-2 / Down
-0x51, // usb 91 key KP-3 / PgDn
-0x4b, // usb 92 key KP-4 / Left
-0x4c, // usb 93 key KP-5
-0x4d, // usb 94 key KP-6 / Right
-0x47, // usb 95 key KP-7 / Home
-0x48, // usb 96 key KP-8 / Up
-0x49, // usb 97 key KP-9 / PgUp
-0x52, // usb 98 key KP-0 / Ins
-0x53, // usb 99 key KP-. / Del
-0x54, // usb 154 key Alt+SysRq
-0x1d, // usb 224 key LCtrl
-0x2a, // usb 225 key LShift
-0x38, // usb 226 key LAlt
-0x5b, // usb 227 key LWin (USB: LGUI) eo-5b
-0x1d, // usb 228 key RCtrl eo-1d
-0x36, // usb 229 key RShift
-0x38, // usb 230 key RAlt eo-38
-0x5c, // usb 231 key RWin (USB: RGUI) eo-5c
+    0,
+    0,
+    0,
+    0,
+    0x1e, // usb 4 key A
+    0x30, // usb 5 key B
+    0x2e, // usb 6 key C
+    0x20, // usb 7 key D
+    0x12, // usb 8 key E
+    0x21, // usb 9 key F
+    0x22, // usb 10 key G
+    0x23, // usb 11 key H
+    0x17, // usb 12 key I
+    0x24, // usb 13 key J
+    0x25, // usb 14 key K
+    0x26, // usb 15 key L
+    0x32, // usb 16 key M
+    0x31, // usb 17 key N
+    0x18, // usb 18 key O
+    0x19, // usb 19 key P
+    0x10, // usb 20 key Q
+    0x13, // usb 21 key R
+    0x1f, // usb 22 key S
+    0x14, // usb 23 key T
+    0x16, // usb 24 key U
+    0x2f, // usb 25 key V
+    0x11, // usb 26 key W
+    0x2d, // usb 27 key X
+    0x15, // usb 28 key Y
+    0x2c, // usb 29 key Z
+    0x02, // usb 30 key 1 !
+    0x03, // usb 31 key 2 @
+    0x04, // usb 32 key 3 #
+    0x05, // usb 33 key 4 $
+    0x06, // usb 34 key 5 % E
+    0x07, // usb 35 key 6 ^
+    0x08, // usb 36 key 7 &
+    0x09, // usb 37 key 8 *
+    0x0a, // usb 38 key 9 (
+    0x0b, // usb 39 key 0 )
+    0x1c, // usb 40 key Enter
+    0x01, // usb 41 key Esc
+    0x0e, // usb 42 key Backspace
+    0x0f, // usb 43 key Tab
+    0x39, // usb 44 key space
+    0x0c, // usb 45 key - _
+    0x0d, // usb 46 key = +
+    0x1a, // usb 47 key [ {
+    0x1b, // usb 48 key ] }
+    0x2b, // usb 49 key \ |
+    0x00, // usb 50 key non-US-1
+    0x27, // usb 51 key ; :
+    0x28, // usb 52 key ' "
+    0x29, // usb 53 key ` ~
+    0x33, // usb 54 key , <
+    0x34, // usb 55 key . >
+    0x35, // usb 56 key / ?
+    0x3a, // usb 57 key CapsLock
+    0x3b, // usb 58 key F1
+    0x3c, // usb 59 key F2
+    0x3d, // usb 60 key F3
+    0x3e, // usb 61 key F4
+    0x3f, // usb 62 key F5
+    0x40, // usb 63 key F6
+    0x41, // usb 64 key F7
+    0x42, // usb 65 key F8
+    0x43, // usb 66 key F9
+    0x44, // usb 67 key F10
+    0x57, // usb 68 key F11
+    0x58, // usb 69 key F12
+    0x37, // usb 70 key PrtScr eo-37
+    0x46, // usb 71 key ScrollLock
+    0xe1, // usb 72 key Pause e1-1d-45
+    0x52, // usb 73 key Insert eo-52
+    0x47, // usb 74 key Home eo-47
+    0x49, // usb 75 key PgUp eo-49
+    0x53, // usb 76 key Delete eo-53
+    0x4f, // usb 77 key End eo-4f
+    0x51, // usb 78 key PgDn eo-51
+    0x4d, // usb 79 key Right eo-4d
+    0x4b, // usb 80 key Left eo-4b
+    0x50, // usb 81 key Down eo-50
+    0x48, // usb 82 key Up eo-48
+    0x45, // usb 83 key NumLock
+    0x35, // usb 84 key KP-/ eo-35
+    0x37, // usb 85 key KP-*
+    0x4a, // usb 86 key KP--
+    0x4e, // usb 87 key KP-+
+    0x1c, // usb 88 key KP-Enter eo-1c
+    0x4f, // usb 89 key KP-1 / End
+    0x50, // usb 90 key KP-2 / Down
+    0x51, // usb 91 key KP-3 / PgDn
+    0x4b, // usb 92 key KP-4 / Left
+    0x4c, // usb 93 key KP-5
+    0x4d, // usb 94 key KP-6 / Right
+    0x47, // usb 95 key KP-7 / Home
+    0x48, // usb 96 key KP-8 / Up
+    0x49, // usb 97 key KP-9 / PgUp
+    0x52, // usb 98 key KP-0 / Ins
+    0x53, // usb 99 key KP-. / Del
+    0x54, // usb 154 key Alt+SysRq
+    0x1d, // usb 224 key LCtrl
+    0x2a, // usb 225 key LShift
+    0x38, // usb 226 key LAlt
+    0x5b, // usb 227 key LWin (USB: LGUI) eo-5b
+    0x1d, // usb 228 key RCtrl eo-1d
+    0x36, // usb 229 key RShift
+    0x38, // usb 230 key RAlt eo-38
+    0x5c, // usb 231 key RWin (USB: RGUI) eo-5c
 };
 
 #endif // XT_DEFINES
